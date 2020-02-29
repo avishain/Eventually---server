887309e3fb28a3e3e41a8f1580b70dc636f22979
Avishai Nudelman <avishainu@gmail.com>
2020-02-29 10:49:21 +0200

    Update index.js
    enable CORS

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 3000;
 
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/users', usersRouter);
 app.use('/events', eventsRouter);
